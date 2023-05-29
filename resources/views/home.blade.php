5ed4376fe7c6ecd16d91014e597fd5349cc7008c
NelloRossi <rossianiello97@gmail.com>
2023-05-29 16:42:59 +0200

    added button to home to get all comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('main_content')
-    <div class="container">
-        main
+    <div class="container d-flex justify-content-center mt-5">
+        <a href="{{ route('comics.index') }}" class="btn btn-primary">Vai alla lista dei fumetti</a>
     </div>
 @endsection
