0e884ce187c5ae986dbe474ce5c8791ccfcee20d
NelloRossi <rossianiello97@gmail.com>
2023-05-29 15:58:30 +0200

    added cards to index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,6 +5,15 @@
 
 @section('main_content')
     <div class="container d-flex my-5 justify-content-center">
-
+        <div class="row justify-content-center">
+            @foreach ($comics as $comic)
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3 my-card">
+                    <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+                    <h2>
+                        <a href="{{ route('comics.show', $comic->id) }}">{{ $comic->title }}</a>
+                    </h2>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
