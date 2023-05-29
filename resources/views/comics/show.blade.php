7878934e78e46f8118d982461493463a8af97f6c
NelloRossi <rossianiello97@gmail.com>
2023-05-29 16:09:01 +0200

    added content to show page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,7 +4,24 @@
 @endsection
 
 @section('main_content')
-    <div class="container d-flex my-5 justify-content-center">
+    <div class="container d-flex my-5 justify-content-center info">
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <div class="pe-5">
+                    <h1>{{ $comic->title }}</h1>
+                    <p>{{ $comic->description }}</p>
+                    <p>Prezzo: {{ $comic->price }}</p>
+                    <p>Serie: {{ $comic->series }}</p>
+                    <p>Data di rilascio: {{ $comic->sale_date }}</p>
+                    <p class="text-capitalize">Tipo: {{ $comic->type }}</p>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+            </div>
+        </div>
+
+
 
     </div>
 @endsection
